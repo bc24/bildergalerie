92493d34a7e976e181029bd68792032571f793e8
bc24 <frank@bc24.org>
2016-02-14 16:10:38 +0100

    Index Datei
    For Schleife erstellt mit den Formart bild1, bild2, bildx....

---
+++ index.php
@@ -0,0 +1,18 @@
+15 Bilder per for Schleife ausgeben!<br>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frank
+ * Date: 14.02.2016
+ * Time: 15:37
+ */
+
+
+$bildnummer = 15;
+for($i = 1; $i <= $bildnummer; $i++)
+{
+    echo '<img src="/bilder/bild'.$i.'.jpg">';
+}
+?>
+
+
